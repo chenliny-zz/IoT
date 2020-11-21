c12e0aacbcedb9e5d160674a0dcb51607a93e94f
CHENLIN YE <CHENLINY@BERKELEY.EDU>
2020-11-21 11:17:29 -0800

    updated launch_broker

--- IoT_101/xavier/broker/launch_broker.sh
+++ IoT_101/xavier/broker/launch_broker.sh
@@ -4,7 +4,7 @@
 docker network create --driver bridge iot101
 
 # Build the docker image based on the broker Dockerfile
-docker build -t broker .
+docker build -t broker -f Dockerfile.edgebroker .
 
 # launch
 docker run -d --network iot101 --name broker -p 1883:1883 -ti broker mosquitto -v
